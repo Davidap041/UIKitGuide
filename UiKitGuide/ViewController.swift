8fc59373e72933cd1c325b447f85083850a93c7d
Davi Paiva <davialexandre_dap@hotmail.com>
2024-12-17 17:38:56 -0300

    use DiffableDataSource

--- UiKitGuide/ViewController.swift
+++ UiKitGuide/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     private lazy var formContentBuilder = FormContentBuilderImpl()
     private lazy var formCompLayout = FormCompositionalLayout()
+    private lazy var dataSource = makeDataSource()
     
     private lazy var collectionView = {
         let cv = UICollectionView(frame: .zero,collectionViewLayout: formCompLayout.layout)
@@ -27,12 +28,15 @@ class ViewController: UIViewController {
     override func loadView() {
         super.loadView()
         setup()
+        updateDataSource()
     }
-
+    
 }
 private extension ViewController {
     func setup() {
         view.backgroundColor = .white
+        
+        collectionView.dataSource = dataSource
         
         // Hierarchy
         view.addSubview(collectionView)
@@ -42,7 +46,51 @@ private extension ViewController {
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        
-        
+    }
+    func makeDataSource() -> UICollectionViewDiffableDataSource<FormSectionComponent, FormComponent> {
+        return UICollectionViewDiffableDataSource(collectionView: collectionView) {
+            collectionView,  IndexPath, item in
+            switch item {
+                // Text Form Component
+            case is TextFormComponent:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FormTextCollectionViewCell.cellId, for: IndexPath) as! FormTextCollectionViewCell
+                cell.bind(item)
+                return cell
+                
+                // Date Form Component
+            case is DateFormComponent:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FormDateCollectionViewCell.cellId, for: IndexPath) as! FormDateCollectionViewCell
+                cell.bind(item)
+                return cell
+                
+                // Button Form Component:
+            case is ButtonFormComponent:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FormButtonCollectionViewCell.cellId, for: IndexPath) as! FormButtonCollectionViewCell
+                cell.bind(item)
+                return cell
+                
+                // Default
+            default:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UICollectionViewCell.cellId, for: IndexPath)
+                return cell
+                
+                
+            }
+        }
+    }
+    func  updateDataSource(animated: Bool = false){
+        DispatchQueue.main.async {
+            [weak self] in
+            
+            guard let self = self else { return }
+            
+            var snapshot = NSDiffableDataSourceSnapshot<FormSectionComponent, FormComponent>()
+            
+            let formSections = self.formContentBuilder.content
+            snapshot.appendSections(formSections)
+            formSections.forEach{snapshot.appendItems($0.items, toSection: $0)}
+            
+            self.dataSource.apply(snapshot,animatingDifferences: animated)
+        }
     }
 }
